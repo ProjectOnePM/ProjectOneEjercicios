56e3ccfbb86ff9c6bc039ec4c39f6d6c14354863
Laura <lau2886@live.com>
2016-04-24 20:36:50 -0300

    Con funcion

--- Unidad 4 - Funciones/Funciones/UT4ejeI.py
+++ Unidad 4 - Funciones/Funciones/UT4ejeI.py
@@ -1,12 +1,15 @@
-n=input("Ingrese el numero: ")
-sum=0
-for i in range(1,(n/2)+1):
-        if n%i == 0:
-            sum=sum+i
-if n==sum:
-	print"Es un numero perfecto"
-elif (n<sum):
-	print"Es un numero abundante"
-elif (n>sum):
-	print"Es un numero deficiente"
+def funcionnumero(n):
+	sum=0
+	for i in range(1,(n/2)+1):
+     		if n%i==0:
+     	   		sum=sum+i
+	if n==sum:
+		return"Es un numero perfecto"
+	elif (n<sum):
+		return"Es un numero abundante"
+	elif (n>sum):
+		return"Es un numero deficiente"
 
+n=input("Ingrese el numero: ")
+numero=funcionnumero(n)
+print numero

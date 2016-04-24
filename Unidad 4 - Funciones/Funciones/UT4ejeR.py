a0ded3daea67d928cfa625c3e1424d43312940dd
FacundoMierez <facundomierez@gmail.com>
2016-04-23 23:30:54 -0300

    Update UT4ejeR.py

--- Unidad 4 - Funciones/Funciones/UT4ejeR.py
+++ Unidad 4 - Funciones/Funciones/UT4ejeR.py
@@ -1,16 +1,41 @@
+def primo_no_primo():
+	opcion="SI"
+	while True:
+		try:
+			print(" ")
+			num=input('INGRESE UN NUMERO NATURAL (NO)--->SALIR: ')
+			
+			if num=='NO' or num=='no':
+				break
+			elif (es_primo(int(num))):
+				print("EL NUMERO %s ES PRIMO" %num , " Y SU FACTORIA ES: " , factorial(int(num)))
+			else:
+				print("EL NUMERO %s NO ES PRIMO" %num, "Y SUS MULTIPLOS SON: ")
+				multiplos_resul=multiplos(int(num))
+				for i in range(0,len(multiplos_resul)):
+					print("NUMERO: ", multiplos_resul[i])
+		except:
+			print(" El NÚMERO TIENE QUE SER UN NUMERO NATURAL")
 
-#ejercicio r)
-def edades(edad1,edad2):
-	if int(edad1)>int(edad2):
-		mayor=edad1
-		distancia=int(edad1)-int(edad2)
-		return mayor,distancia
-	elif int(edad2)>int(edad1):
-		mayor=edad2
-		distancia=int(edad2)-int(edad1)
-		return mayor,distancia
-	else:
-		return "SON IGUALES"
 
+def es_primo(var):
+	for i in range(2,var):
+		if (var%i)==0:
+			return False
+		else:
+			return True
+
+def factorial(var):
+	fact=1
+	for i in range(1,var):
+		fact= fact + (fact*i)
+	return fact
+
+def multiplos(var):
+	lista_multiplos=[]
+	for i in range(1,var):
+		if var%i == 0:
+			lista_multiplos.append(i)
+	return lista_multiplos
 
 
